cb06589ea52713d7216400c3293f7aa21cb7fe88
Sushant Sharma <sushant.wurk@gmail.com>
2025-03-12 21:50:09 +0530

    alias

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,13 +5,14 @@ set -gx COLORTERM truecolor
 set -gx HOMEBREW_PREFIX "/home/linuxbrew/.linuxbrew"
 set -gx HOMEBREW_CELLAR "/home/linuxbrew/.linuxbrew/Cellar"
 set -gx HOMEBREW_REPOSITORY "/home/linuxbrew/.linuxbrew/Homebrew"
-set -gx PATH "/home/linuxbrew/.linuxbrew/bin" "/home/linuxbrew/.linuxbrew/sbin" "/usr/sbin" "/home/sushi/.config/emacs/bin" "/home/sushi/.local/bin" $PATH
+set -gx PATH "/home/linuxbrew/.linuxbrew/bin" "/home/linuxbrew/.linuxbrew/sbin" "/usr/sbin" "/home/sushi/.config/emacs/bin" "/home/sushi/.local/bin" "/home/sushi/development/flutter/bin" "/home/sushi/git/emsdk/upstream/bin" $PATH
 set -q MANPATH; or set MANPATH ''
 set -gx MANPATH "/home/linuxbrew/.linuxbrew/share/man" $MANPATH
 set -q INFOPATH; or set INFOPATH ''
 set -gx INFOPATH "/home/linuxbrew/.linuxbrew/share/info" $INFOPATH
 set -gx EDITOR "nvim"
 
+alias emsdk_setup ". ~/git/emsdk/emsdk_env.fish"
 alias pbcopy="xclip -selection clipboard"
 alias pbpaste="xclip -selection clipboard -o"
 alias dev="cd ~/nix-home/sushi/dev/"
