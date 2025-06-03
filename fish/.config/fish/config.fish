98bb4b30fbf4704e0db9f20830b4e21859dc8523
Sushant Sharma <sushant.wurk@gmail.com>
2025-06-04 01:07:40 +0530

    fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,28 +2,25 @@ set fish_greeting ""
 set -gx COLORTERM truecolor
 
 
-set -gx HOMEBREW_PREFIX "/home/linuxbrew/.linuxbrew"
-set -gx HOMEBREW_CELLAR "/home/linuxbrew/.linuxbrew/Cellar"
-set -gx HOMEBREW_REPOSITORY "/home/linuxbrew/.linuxbrew/Homebrew"
+# set -gx HOMEBREW_PREFIX "/home/linuxbrew/.linuxbrew"
+# set -gx HOMEBREW_CELLAR "/home/linuxbrew/.linuxbrew/Cellar"
+# set -gx HOMEBREW_REPOSITORY "/home/linuxbrew/.linuxbrew/Homebrew"
 
-set -gx PATH "/home/linuxbrew/.linuxbrew/bin" "/home/linuxbrew/.linuxbrew/sbin" "/usr/sbin" "/home/sushi/.config/emacs/bin" "/home/sushi/.local/bin" "/home/sushi/development/flutter/bin" "/home/sushi/git/emsdk/upstream/bin" "/home/sushi/.dotnet/" "/home/sushi/.local/share/nvim/mason/bin" "/home/sushi/Android/Sdk/platform-tools" "/home/sushi/.local/share/nvim/mason/bin" $PATH
+set -gx PATH "/usr/sbin" "/home/sushi/.local/bin" "/home/sushi/development/flutter/bin" "/home/sushi/.local/share/nvim/mason/bin" "/home/sushi/Android/Sdk/platform-tools" "/home/sushi/.local/share/nvim/mason/bin" "/home/sushi/.asdf/shims/" "/home/sushi/Android/Sdk/cmdline-tools/latest/bin" $PATH
 
-set -q MANPATH; or set MANPATH ''
-set -gx MANPATH "/home/linuxbrew/.linuxbrew/share/man" $MANPATH
-set -q INFOPATH; or set INFOPATH ''
-set -gx INFOPATH "/home/linuxbrew/.linuxbrew/share/info" $INFOPATH
+set -gx CHROME_EXECUTABLE "/usr/sbin/google-chrome-stable"
 set -gx EDITOR "nvim"
 set -gx ANDROID_HOME "/home/sushi/Android/Sdk"
 
-alias emsdk_setup ". ~/git/emsdk/emsdk_env.fish"
+set -gx PATH /home/sushi/.local/share/mise/shims $PATH
+
+# alias emsdk_setup ". ~/git/emsdk/emsdk_env.fish"
 alias pbcopy="xclip -selection clipboard"
 alias pbpaste="xclip -selection clipboard -o"
-alias dev="cd ~/nix-home/sushi/dev/"
+# alias dev="cd ~/nix-home/sushi/dev/"
 alias p="pnpm"
-alias dots="cd ~/.dotfiles"
+alias dots="cd ~/.dots"
 alias emacs="emacs -nw"
-alias z="zypper"
-alias sz="sudo zypper"
 
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
@@ -37,3 +34,6 @@ if not string match -q -- $PNPM_HOME $PATH
   set -gx PATH "$PNPM_HOME" $PATH
 end
 # pnpm end
+
+# mise activate fish | source
+mise activate fish | source
