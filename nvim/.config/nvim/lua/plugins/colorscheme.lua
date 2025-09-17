02f17457b3dfc124edf7e516494db96d5c3a70c5
Sushant Sharma <sushant.wurk@gmail.com>
2025-09-17 22:17:40 +0530

    colorscheme stuff

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,35 +1,43 @@
+local hour = function()
+  return os.date('*t').hour
+end
+
 return {
-  {
-    'sainnhe/gruvbox-material',
-    -- config = function()
-    --   vim.g.gruvbox_material_enable_italic = true
-    --   vim.g.gruvbox_material_background = 'hard'
-    --   vim.cmd [[colorscheme gruvbox-material]]
-    -- end,
-  },
   {
     'folke/tokyonight.nvim',
     lazy = false,
     priority = 1000,
     opts = {},
     config = function()
-      -- vim.cmd [[colorscheme tokyonight-night]]
-    end,
-  },
-  {
-    'vague2k/vague.nvim',
-    config = function()
-      -- vim.cmd [[colorscheme vague]]
+      if hour() >= 18 or hour() < 12 then
+        vim.cmd [[colorscheme tokyonight-night]]
+      end
     end,
   },
   {
     'rose-pine/neovim',
     name = 'rose-pine',
     config = function()
-      vim.o.background = 'light'
-      vim.cmd 'colorscheme rose-pine'
+      if 12 < hour() and hour() < 18 then
+        vim.o.background = 'light'
+        vim.cmd 'colorscheme rose-pine'
+      end
     end,
   },
+  -- {
+  -- 'sainnhe/gruvbox-material',
+  -- config = function()
+  --   vim.g.gruvbox_material_enable_italic = true
+  --   vim.g.gruvbox_material_background = 'hard'
+  --   vim.cmd [[colorscheme gruvbox-material]]
+  -- end,
+  -- },
+  -- {
+  --   'vague2k/vague.nvim',
+  --   config = function()
+  --     -- vim.cmd [[colorscheme vague]]
+  --   end,
+  -- },
   -- {
   --   'shaunsingh/solarized.nvim',
   --   config = function()
